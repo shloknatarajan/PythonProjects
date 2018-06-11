0a0169fc854636f993dff17e97215860d69ebcc4
Shlok Natarajan <snatarajan542@rsdmo.org>
2018-06-11 11:58:14 -0500

    followercheck working

--- Twitter Follow Project/followercheck.py
+++ Twitter Follow Project/followercheck.py
@@ -6,36 +6,35 @@ from config import consumer_key_2, consumer_secret_2, access_token_2, access_tok
 # uses snakeresearcher2
 
 # Authorizations for tweepy
-auth2 = tweepy.OAuthHandler(consumer_key_2, consumer_secret_2)
-auth2.set_access_token(access_token_2, access_token_secret_2)
-api = tweepy.API(auth2)
+auth = tweepy.OAuthHandler(consumer_key_2, consumer_secret_2)
+auth.set_access_token(access_token_2, access_token_secret_2)
+api = tweepy.API(auth)
 
-Prelim = open("prelim.txt", "r").readlines()
+#Prelim = open("prelim.txt", "r").readlines()
+def one_follower_check(name):
+    person = api.get_user(name)
+    if(person.followers_count < 150):
+        # print("User has less than 150 followers")
+        # print(person.screen_name + " has " + str(person.followers_count) + " followers.")
+        return False
+    else:
+        # print(person.screen_name + " has " + str(person.followers_count) + " followers (sufficient)")
+        return True
 
-def follower_check():
-    index = 0
-    # Converts screen name doc to a python list
+def write_one_follower(name):
+    if(one_follower_check(name) == True):
+        with open("temp.txt", "a") as temp:
+            temp.write(name)
+
+def main():
+    Prelim = open("prelim.txt", "r").readlines()
     for user in Prelim:
-        person = tweepy.api.get_user(user)
-        if (person.followers_count < 150):
-            del(Prelim[index])
-            print("deleting person")
-        else:
-            print(person.screen_name + "has " + person.followers_count + " so they will not be removed \n")
-            index += 1
-    
-def write_to_temp():
-    # Writes sorted list to a temp file
-    with open("temp.txt", "w") as temp:
-        for user in Prelim:
-            temp.write(user)
+        write_one_follower(user)
+
+main()
+
+
         
-try:
-    follower_check()
-    print("Finished check. Writing to temp")
-    write_to_temp()
-except:
-    print("writing to temp prematurely")
-    write_to_temp()
+
 
         
